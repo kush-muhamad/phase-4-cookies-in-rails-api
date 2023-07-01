171460c4c01b4b7b1beda32cfbefa0f2b50e252f
kush Muhamad <kushmohammed98@yahoo.com>
2023-07-01 13:00:32 +0300

    completed task

--- config/application.rb
+++ config/application.rb
@@ -19,6 +19,22 @@ require "rails/test_unit/railtie"
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
+module MyApp
+  class Application < Rails::Application
+    config.load_defaults 6.1
+    # This is set in apps generated with the --api flag, and removes session/cookie middleware
+    config.api_only = true
+
+    # Must add these lines!
+    # Adding back cookies and session middleware
+    config.middleware.use ActionDispatch::Cookies
+    config.middleware.use ActionDispatch::Session::CookieStore
+
+    # Use SameSite=Strict for all cookies to help protect against CSRF
+    config.action_dispatch.cookies_same_site_protection = :strict
+  end
+end
+
 module Phase4CookiesInRailsApi
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
@@ -44,4 +60,5 @@ module Phase4CookiesInRailsApi
     # Use SameSite=Strict for all cookies to help protect against CSRF
     config.action_dispatch.cookies_same_site_protection = :strict
   end
+
 end
